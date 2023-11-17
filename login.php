2df1462dd44c33867d6dbf252a38b4f74b985420
eigu <jmpheen@gmail.com>
2023-11-17 14:48:44 +0800

    form validation

--- login.php
+++ login.php
@@ -1,66 +1,39 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta charset="UTF-8">
-        <title>Kape Para Sayo</title>
-        <link rel="icon" type="image/x-icon" href="assets/logo/icon_01.png">    
+<?php
+session_start ();
 
-        <link rel="stylesheet" href="css/styles.css">
-        <script src="js/scripts.js"></script>
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    if (validateLogin($_POST['username'], $_POST['password'])) {
+        $_SESSION['login'] = "1";
+        $_SESSION['username'] = $_POST['username'];
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    </head>
+        echo '<script>
+            alert("Login successful!");
+            window.location.href = "index.html";
+        	</script>';
+    } else {
+        echo '<script>
+			alert("Invalid Username or Password!");
+			window.location.href = "login.html";
+			</script>';
+    }
+}
 
-    <body>
-        <main class="container">
-            <div class="container d-flex flex-column min-vh-100 justify-content-md-start align-items-center" style="max-width: 1200px; max-height: 800px;">
-                <div class="row align-items-center rounded-3 shadow-lg my-auto hero">
-                    <div class="col-md-6 col-lg-5 d-none d-md-block p-0 shadow-lg">
-                        <img src="assets/stock/person_01.jpg"
-                        alt="login form" class="img-fluid" style="border-radius: .3rem 0 0 .3rem;" />
-                    </div>
-                    <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                        <div class="card-body p-4 p-lg-5 text-black">
-                            <form name="frmContact" method="post" action="loginprocess.php" style="max-width: 600px; height: 500px;">
-                                <a class="navbar-brand" href="index.html#home">
-                                    <img class="rounded-lg-3 d-block mb-4"  style="height: 85px;" src="assets/logo/logo_01.png" alt="">
-                                </a>
-            
-                                <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-            
-                                <div class="form-outline mb-4">
-                                <input type="username" class="form-control form-control-lg" />
-                                <label class="form-label">Username</label>
-                                </div>
-            
-                                <div class="form-outline mb-4">
-                                <input type="password" class="form-control form-control-lg" />
-                                <label class="form-label">Password</label>
-                                </div>
-            
-                                <div class="pt-1 mb-4">
-                                <input class="btn btn-dark btn-lg btn-block" type="submit" name="Login" id="Login" value="Login">
-                                </div>
+function validateLogin($username, $password) {
+	$con = mysqli_connect('localhost', 'root', '','online_store');
+    $query = "SELECT * FROM profile WHERE username='$username'";
 
-                                <?php 
-                                if(isset($_REQUEST["err"]))
-                                    $msg="Invalid username or Password";
-                                ?>
-                                <p style="color:red;">
-                                <?php if(isset($msg))
-                                {
-                                echo $msg;
-                                }
-                                ?>
-                                </p>
+    $result = mysqli_query($con, $query);
 
-                                <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="register.html" style="color: #393f81;">Register here</a></p>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </body>
-</html>
+    if ($result->num_rows == 0) {
+        return false;
+    }
+
+    $user = $result->fetch_assoc();
+
+    if (!password_verify($password, $user['password'])) {
+        return false;
+    }
+
+    return true;
+}
+?>
--- register.php
+++ register.php
@@ -1,23 +1,48 @@
 <?php
+session_start();
 
-$con = mysqli_connect('localhost', 'root', '','online_store');
+if (isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['mobile']) && isset($_POST['address'])) {
+	if (registerUser($_POST['email'], $_POST['username'],$_POST['password'], $_POST['firstname'],  $_POST['lastname'], $_POST['mobile'], $_POST['address'])) {
+		$_SESSION['login'] = "1";
+		$_SESSION['username'] = $_POST['username'];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$mobile = $_POST['mobile'];
-$address = $_POST['address'];
-$email = $_POST['email'];
-
-$sql = "INSERT INTO `profile` (`username`, `password`, `firstname`, `lastname`, `mobile`, `address`, `email`) VALUES ('$username', '$password', '$firstname', '$lastname', '$mobile', '$address', '$email')";
-
-// insert in database 
-$rs = mysqli_query($con, $sql);
-
-if($rs)
-{
-	echo "Contact Records Inserted";
+		echo '<script>
+			alert("Registration successful!");
+			window.location.href = "index.html";
+			</script>';
+	}
 }
+	
+function registerUser($email, $username, $password, $firstname, $lastname, $mobile, $address,) {
+	$con = mysqli_connect('localhost', 'root', '','online_store');
 
+    $usernameQuery = "SELECT * FROM profile WHERE username='$username'";
+    $usernameResult = mysqli_query($con, $usernameQuery);
+
+    $emailQuery = "SELECT * FROM profile WHERE email='$email'";
+    $emailResult = mysqli_query($con, $emailQuery);
+
+    if (mysqli_num_rows($usernameResult) > 0 || mysqli_num_rows($emailResult) > 0) {
+        if (mysqli_num_rows($usernameResult) > 0) {
+			echo '<script>
+				alert("Username already exists!");
+				window.location.href = "register.html";
+				</script>';
+        }
+
+        if (mysqli_num_rows($emailResult) > 0) {
+			echo '<script>
+				alert("Email already exists!");
+				window.location.href = "register.html";
+				</script>';
+        }
+
+        return false;
+    }
+
+	$query = "INSERT INTO `profile` (`email`, `username`, `password`, `firstname`, `lastname`, `mobile`, `address`) VALUES ('$email', '$username', '$password', '$firstname', '$lastname', '$mobile', '$address')";
+	$result = mysqli_query($con, $query);
+
+    return true;
+}
 ?>
